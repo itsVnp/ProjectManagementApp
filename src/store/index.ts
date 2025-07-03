7e51e89ddb8a41d3092c702cb9d187e0f064277b
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-07-03 09:00:00 +0000

    refactor: restructure project components

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,3 +6,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-06-23 (commit 1)
 // Modified on: 2025-06-27 (commit 5)
 // Modified on: 2025-06-30 (commit 7)
+// Modified on: 2025-07-03 (commit 1)
