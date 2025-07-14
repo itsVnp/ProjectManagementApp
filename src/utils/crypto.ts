1a1ca7d8a2fa4072ccb9b864a04ad057ff9f0621
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-07-14 09:00:00 +0000

    style: enhance dark mode support

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -6,3 +6,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-07-01 (commit 7)
 // Modified on: 2025-07-07 (commit 6)
 // Modified on: 2025-07-10 (commit 1)
+// Modified on: 2025-07-14 (commit 8)
