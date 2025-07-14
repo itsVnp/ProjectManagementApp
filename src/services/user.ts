4101ed0ea59a6fb04b97f5fb7115469ce7c30294
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-07-14 09:00:00 +0000

    fix: correct mobile app navigation bug

--- src/services/user.ts
+++ src/services/user.ts
@@ -6,3 +6,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-06-25 (commit 1)
 // Modified on: 2025-06-26 (commit 5)
 // Modified on: 2025-06-28 (commit 2)
+// Modified on: 2025-07-14 (commit 5)
