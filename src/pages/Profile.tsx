1b187ac82b3ccfca767eca4c39cc8706ecf0b872
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-07-25 09:00:00 +0000

    perf: optimize image loading

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -8,3 +8,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-07-08 (commit 5)
 // Modified on: 2025-07-13 (commit 1)
 // Modified on: 2025-07-20 (commit 1)
+// Modified on: 2025-07-25 (commit 5)
