b9a4dc004f7f6305c56e22ac6ca490be8739ed81
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-07-27 09:00:00 +0000

    style: enhance mobile responsive design

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -7,3 +7,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-07-07 (commit 4)
 // Modified on: 2025-07-14 (commit 2)
 // Modified on: 2025-07-21 (commit 3)
+// Modified on: 2025-07-27 (commit 3)
