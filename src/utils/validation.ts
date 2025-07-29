92829bdbe591b29eb32f8dd9354453ce635c09ec
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-07-29 09:00:00 +0000

    style: enhance mobile responsive design

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -6,3 +6,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-06-25 (commit 8)
 // Modified on: 2025-06-30 (commit 4)
 // Modified on: 2025-07-08 (commit 4)
+// Modified on: 2025-07-29 (commit 6)
