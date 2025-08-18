6ee295a2e1acbbae2d56a4d7bde01d7869fe7a45
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-08-18 09:00:00 +0000

    feat: add project management dashboard

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -9,3 +9,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-07-08 (commit 8)
 // Modified on: 2025-08-06 (commit 3)
 // Modified on: 2025-08-07 (commit 1)
+// Modified on: 2025-08-18 (commit 5)
