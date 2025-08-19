16398ce187c5a445f35f22aafbbec27cc1bde37d
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-08-19 09:00:00 +0000

    feat: add new user authentication system

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -10,3 +10,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-07-27 (commit 3)
 // Modified on: 2025-08-04 (commit 1)
 // Modified on: 2025-08-11 (commit 2)
+// Modified on: 2025-08-19 (commit 4)
