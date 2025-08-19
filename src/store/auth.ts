38fd987a2aa676c91a2d434e4ca69342bda48411
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-08-19 09:00:00 +0000

    refactor: restructure project components

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -10,3 +10,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-07-15 (commit 2)
 // Modified on: 2025-08-04 (commit 8)
 // Modified on: 2025-08-05 (commit 1)
+// Modified on: 2025-08-19 (commit 8)
