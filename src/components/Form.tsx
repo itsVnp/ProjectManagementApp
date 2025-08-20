dc22c74c25e1a33742c27f51777f7672541b3b6e
Vivekanand Pandey <itsvnpandey@gmail.com>
2025-08-20 09:00:00 +0000

    docs: add setup instructions

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,3 +9,4 @@ export const placeholder = 'This is a placeholder file';
 // Modified on: 2025-08-01 (commit 4)
 // Modified on: 2025-08-08 (commit 4)
 // Modified on: 2025-08-12 (commit 2)
+// Modified on: 2025-08-20 (commit 1)
